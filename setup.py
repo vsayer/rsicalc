f971e8aff67879e0c9ae0a7828999234d38e92bf
Vivek Ayer <ayer@pobox.com>
2016-02-09 12:28:49 -0800

    add license and readme to packaging
    - add README.md
    - add LICENSE

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ setup(name='rsicalc',
       author_email='ayer@pobox.com',
       url='https://github.com/vsayer/rsicalc',
       license='BSD',
+      package_data={'': ['README.md', 'LICENSE']},
+      include_package_data=True,
       install_requires=['yahoo_finance'],
       scripts=['bin/rsicalc'],
       )
